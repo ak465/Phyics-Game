e942a03b3b0a347cbe716208c2db688c206167e6
ak465 <76836592+ak465@users.noreply.github.com>
2021-12-24 21:03:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,3 @@
-// Angry Birds
-// Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/138-angry-birds.html
-// https://youtu.be/TDQzoe9nslY
-
 const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
 
 let ground;
